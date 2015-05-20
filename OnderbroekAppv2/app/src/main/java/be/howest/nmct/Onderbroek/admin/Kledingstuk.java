8ae5624b1438fb4166bdc0acf9296fa6d860ff0d
Joren <jorenvandamme@hotmail.com>
2015-05-20 10:50:37 +0200

    Paar dingen aangepast
    Mogelijk werkt het niet meer.

--- OnderbroekAppv2/app/src/main/java/be/howest/nmct/Onderbroek/admin/Kledingstuk.java
+++ OnderbroekAppv2/app/src/main/java/be/howest/nmct/Onderbroek/admin/Kledingstuk.java
@@ -5,6 +5,7 @@ package be.howest.nmct.Onderbroek.admin;
  */
 public class Kledingstuk {
     private String naamKledingstuk, beschrijvingKledingstuk, plaatsKledingstuk;
+    double latKledingstuk, lonKledingstuk;
 
     public Kledingstuk(String naamKledingstuk)
     {
@@ -12,12 +13,14 @@ public class Kledingstuk {
         this.naamKledingstuk = naamKledingstuk;
     }
 
-    public Kledingstuk(String naamKledingstuk, String beschrijvingKledingstuk, String plaatsKledingstuk )
+    public Kledingstuk(String naamKledingstuk, String beschrijvingKledingstuk, String plaatsKledingstuk, double latKledingstuk, double lonKledingstuk)
     {
         super();
         this.naamKledingstuk = naamKledingstuk;
         this.beschrijvingKledingstuk = beschrijvingKledingstuk;
         this.plaatsKledingstuk = plaatsKledingstuk;
+        this.latKledingstuk = latKledingstuk;
+        this.lonKledingstuk = lonKledingstuk;
     }
 
     public String getNaamKledingstuk() {
@@ -42,5 +45,21 @@ public class Kledingstuk {
 
     public void setPlaatsKledingstuk(String plaatsKledingstuk) {
         this.plaatsKledingstuk = plaatsKledingstuk;
+    }
+
+    public double getLatKledingstuk() {
+        return latKledingstuk;
+    }
+
+    public double getLonKledingstuk() {
+        return lonKledingstuk;
+    }
+
+    public void setLatKledingstuk(double latKledingstuk) {
+        this.latKledingstuk = latKledingstuk;
+    }
+
+    public void setLonKledingstuk(double lonKledingstuk) {
+        this.lonKledingstuk = lonKledingstuk;
     }
 }
