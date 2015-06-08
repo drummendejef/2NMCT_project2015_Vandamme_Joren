7692448abb3a7dd818f3173649ff104db693d31d
Joren <jorenvandamme@hotmail.com>
2015-06-08 08:51:49 +0200

    PlaatsenFragment half zichtbaar
    Enkel de coordinaten moeten nog toegevoegd worden.

--- OnderbroekAppv2/app/src/main/java/be/howest/nmct/Onderbroek/PlaatsenFragment.java
+++ OnderbroekAppv2/app/src/main/java/be/howest/nmct/Onderbroek/PlaatsenFragment.java
@@ -1,17 +1,28 @@
 package be.howest.nmct.Onderbroek;
 
 
+import android.app.ListFragment;
+import android.app.LoaderManager;
+import android.content.Context;
+import android.content.Loader;
+import android.database.Cursor;
 import android.os.Bundle;
 import android.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.SimpleCursorAdapter;
+
+import be.howest.nmct.Onderbroek.loader.Contract;
+import be.howest.nmct.Onderbroek.loader.KledingLoader;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class PlaatsenFragment extends Fragment {
+public class PlaatsenFragment extends ListFragment implements LoaderManager.LoaderCallbacks<Cursor> {
+    PlaatsenAdapter mAdapter;
+
 
 
     public PlaatsenFragment() {
@@ -30,5 +41,51 @@ public class PlaatsenFragment extends Fragment {
         return inflater.inflate(R.layout.fragment_plaatsen, container, false);
     }
 
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
 
+        mAdapter  = new PlaatsenAdapter(getActivity(),
+                R.layout.row_plaatsen,
+                null,
+                new String[]{
+                        Contract.KledijColumns.COLUMN_KLEDING_PLAATS,
+                },
+                new int[]{R.id.textViewLocatieNaam},0);
+
+        setListAdapter(mAdapter);
+
+        getLoaderManager().initLoader(0, null, this);
+    }
+
+    @Override
+    public Loader<Cursor> onCreateLoader(int id, Bundle args) {
+        return new KledingLoader(getActivity());
+    }
+
+    @Override
+    public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
+        mAdapter.swapCursor(data);
+    }
+
+    @Override
+    public void onLoaderReset(Loader<Cursor> loader) {
+        mAdapter.swapCursor(null);
+    }
+
+
+    class PlaatsenAdapter extends SimpleCursorAdapter {
+        public PlaatsenAdapter(Context context, int layout, Cursor c, String[] from, int[] to, int flags) {
+            super(context, layout, c, from, to, flags);
+        }
+
+        @Override
+        public void bindView(View view, Context context, Cursor cursor) {
+            super.bindView(view, context, cursor);
+
+
+
+
+        }
+    }
 }
